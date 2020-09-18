13f540992bc38752a7ecdebdb577523deb4eab3c
ducmanh27101994 <ducmanh27101994@gmail.com>
2020-09-18 14:31:54 +0700

    validate

--- app/Http/Controllers/AlbumController.php
+++ app/Http/Controllers/AlbumController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AlbumRequest;
 use App\Models\Album;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -38,7 +39,7 @@ class AlbumController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(AlbumRequest $request)
     {
         //
         $album = new Album();
@@ -84,7 +85,7 @@ class AlbumController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(AlbumRequest $request, $id)
     {
         //
         $album = Album::findOrFail($id);
--- resources/views/admin/categorys/create.blade.php
+++ resources/views/admin/categorys/create.blade.php
@@ -10,12 +10,19 @@
             <div class="form-group">
                 <label for="exampleInputEmail1">Category name:</label>
                 <input name="category_name" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                @if($errors->has('category_name'))
+                    <p class="text-danger">{{$errors->first('category_name')}}</p>
+                @endif
             </div>
 
             <div class="form-group">
                 <label for="exampleFormControlFile1">Image: </label>
                 <input name="image" type="file" class="form-control-file" id="exampleFormControlFile1">
+                @if($errors->has('image'))
+                    <p class="text-danger">{{$errors->first('image')}}</p>
+                @endif
             </div>
+
 
             <button type="submit" class="btn btn-info">Add Category</button>
             <button id="back-add" class="btn btn-secondary" onclick="window.history.go(-1); return false;">Cancel</button>
