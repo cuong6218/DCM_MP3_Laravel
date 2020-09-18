88a7b6e14ec5e05a650f2aaa033386f7bf3c0c55
cuong6218 <cuong6218@gmail.com>
2020-09-18 09:52:55 +0700

    create singer table

--- resources/views/admin/layout/master.blade.php
+++ resources/views/admin/layout/master.blade.php
@@ -67,8 +67,8 @@
                     </a>
                     <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-parent="#sidenavAccordion">
                         <nav class="sb-sidenav-menu-nested nav">
-                            <a class="nav-link" href="layout-static.html">List</a>
-                            <a class="nav-link" href="layout-sidenav-light.html">Add new</a>
+                            <a class="nav-link" href="{{route('categorys.index')}}">List</a>
+                            <a class="nav-link" href="{{route('categorys.create')}}">Add new</a>
                         </nav>
                     </div>
 
@@ -78,29 +78,9 @@
                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                     </a>
                     <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-parent="#sidenavAccordion">
-                        <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
-                            <a class="nav-link collapsed"  data-toggle="collapse" data-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                List
-{{--                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>--}}
-                            </a>
-{{--                            <div class="collapse" id="pagesCollapseAuth" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">--}}
-{{--                                <nav class="sb-sidenav-menu-nested nav">--}}
-{{--                                    <a class="nav-link" href="login.html">Login</a>--}}
-{{--                                    <a class="nav-link" href="register.html">Register</a>--}}
-{{--                                    <a class="nav-link" href="password.html">Forgot Password</a>--}}
-{{--                                </nav>--}}
-{{--                            </div>--}}
-                            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                Add new
-{{--                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>--}}
-                            </a>
-{{--                            <div class="collapse" id="pagesCollapseError" aria-labelledby="headingOne" data-parent="#sidenavAccordionPages">--}}
-{{--                                <nav class="sb-sidenav-menu-nested nav">--}}
-{{--                                    <a class="nav-link" href="401.html">401 Page</a>--}}
-{{--                                    <a class="nav-link" href="404.html">404 Page</a>--}}
-{{--                                    <a class="nav-link" href="500.html">500 Page</a>--}}
-{{--                                </nav>--}}
-{{--                            </div>--}}
+                        <nav class="sb-sidenav-menu-nested nav">
+                            <a class="nav-link" href="{{route('singers.index')}}">List</a>
+                            <a class="nav-link" href="layout-sidenav-light.html">Add new</a>
                         </nav>
                     </div>
 
